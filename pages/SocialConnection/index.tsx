1f12c84fcdfac7a82f71ee9a3e211278c959ced9
AbdulhadiJarad <abdulhadi.jarad@gmail.com>
2022-08-23 17:53:57 +0300

    design

--- pages/SocialConnection/index.tsx
+++ pages/SocialConnection/index.tsx
@@ -22,21 +22,7 @@ const SocialConnection: NextPage = () => {
         </div>
         <div className="vl"></div>
       </div>
-      <div style={{ position: "fixed", bottom: 0, marginRight: 50, right: 0 }}>
-        <div style={{ marginTop: "20px" }}>
-          <InstagramIcon />
-        </div>
-        <div style={{ marginTop: "20px" }}>
-          <Gitlab />
-        </div>
-        <div style={{ marginLeft: "-10px", marginTop: "20px" }}>
-          <Linkedin />
-        </div>
-        <div style={{ marginLeft: "-10px", marginTop: "20px" }}>
-          <Email />
-        </div>
-        <div className="vl"></div>
-      </div>
+
     </>
   );
 };
