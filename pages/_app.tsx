3e05caaaa7f99d4e0a5daae9a34a402aea8d9b5e
AbdulhadiJarad9 <Abdulhadi,jarad@agroses.com>
2023-01-17 16:23:38 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,14 +12,14 @@ function MyApp({ Component, pageProps }: AppProps) {
       <meta name="viewport" content="initial-scale=1, width=device-width" />
       <meta name="robots" content="index, follow" />
       <meta name="keywords" content="software engineer, software engineering, abdulhadi jarad,  blockchain, frontend, backend, fullstack, developer, damascus university "></meta>
-      <meta property="og:image" content="/publicImage.png" />
+      <meta property="og:image" content="publicImage.png" />
       <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:alt" content="/publicImage.png" />
-      <meta property="thumbnail" content="/publicImage.png" />
-      <meta property="image" content={'/publicImage.png'} />
+      <meta property="og:image:alt" content="publicImage.png" />
+      <meta property="thumbnail" content="publicImage.png" />
+      <meta property="image" content={'publicImage.png'} />
       <meta property="og:title" content={'Abdulhadi Jarad'} />
-      <meta property="og:image:secure" content={'/publicImage.png'} />
-      <meta property="og:image:url" content={'/publicImage.png'} />
+      <meta property="og:image:secure" content={'publicImage.png'} />
+      <meta property="og:image:url" content={'publicImage.png'} />
       <meta property="og:site_name" content="Abdulhadi Jarad" />
       <meta property="og:description" content="Diligent software full-stack engineer with four years of experience. I Developed and maintained Web Applications with React" />
       <meta property="og:type" content="website" />
